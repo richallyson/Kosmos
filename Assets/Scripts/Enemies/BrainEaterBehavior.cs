bcb08c6600b1eb0057971e2a470785f8dad3bd75
David Oliveira <clear844@gmail.com>
2018-09-09 19:45:49 -0300

    criado gameobject do BrainEater e alteração no EnemyLifeController.cs

---
+++ Assets/Scripts/Enemies/BrainEaterBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainEaterBehavior : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb;
+    
+    private Animator ani;
+    public float EatDelay;
+    private float lastEat;
+    public bool eating;
+    
+    
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        
+        ani = GetComponent<Animator>();
+        rb.velocity = new Vector2(-speed, 0);
+        lastEat = Time.time;
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Time.time - lastEat > EatDelay) {
+            ani.SetTrigger("Eat");
+            lastEat = Time.time;
+        }
+        print(ani.GetCurrentAnimatorStateInfo(0).IsName("Eating"));
+        eating = ani.GetCurrentAnimatorStateInfo(0).IsName("Eating");
+        if (eating) {
+            rb.velocity = new Vector2(-2*speed, 0);
+        }
+
+	}
+
+
+}
