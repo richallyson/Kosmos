bf293a918e17aca43e4ddf8baca80ab5cb14e4aa
David Oliveira <clear844@gmail.com>
2018-09-12 22:04:20 -0300

    tela da logo pronta

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour {
+    private SpriteRenderer sr;
+    private Color original;
+    public Gradient fade;
+    public float start;
+    public float duration;
+    private float timer;
+    public bool done;
+    // Use this for initialization
+	void Start () {
+        timer = 0;
+        done = false;
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer > start && timer < duration) {
+            sr.color = fade.Evaluate((timer - start)/duration);
+        }
+        if(timer >= duration) {
+            done = true;
+        }
+	}
+}
